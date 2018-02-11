1928331000935478919bbfce85e168065b963db6
AroSaneD <mail.junk.3135@gmail.com>
2018-02-11 22:07:03 +0200

    added child class

---
+++ src/app/model/testChildObject.ts
@@ -0,0 +1,8 @@
+export class TestChildObject {
+
+    constructor(
+        public name: string,
+        public nr1: number,
+        public state?: boolean
+    ) { }
+}
--- src/app/model/testObject.ts
+++ src/app/model/testObject.ts
@@ -1,9 +1,11 @@
+import { TestChildObject } from './testChildObject';
 export class TestObject {
-    
+
     constructor(
         public name: string,
         public nr1: number,
-        public nr2: number
+        public nr2: number,
+        public children: TestChildObject[]
     ) {
 
     }
